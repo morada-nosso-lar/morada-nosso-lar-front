06f6b957c038664bb4dcdb7140ba6be36b5ec46c
alexalves011 <alexalves.work@gmail.com>
2026-09-04 12:48:37 -0300

    Projeto Inicial

---
+++ src/Components/Form/index.jsx
@@ -0,0 +1,27 @@
+import Box from "@mui/material/Box";
+
+export default function Form() {
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundColor: "#1976D9",
+          width: "300px",
+          height: "300px",
+          borderRadius: 2,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+
+        <Box >
+
+
+        </Box>
+
+
+
+      </Box>
+    </>
+  );
+}
---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import Form from "../../Components/Form/index";
+
+function Login() {
+  return (
+    <>
+      <Form />
+    </>
+  );
+}
+
+export default Login;
