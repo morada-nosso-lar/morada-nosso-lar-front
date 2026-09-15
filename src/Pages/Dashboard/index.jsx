0e0c1d92c83321d3b59baf555db8b5fd856fb4d2
alexalves011 <alexalves.work@gmail.com>
2026-09-15 13:34:09 -0300

    feat: implementar layout base e sidebar de navegacao

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,14 +1,13 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/AuthContext";
+
 
 export default function Dashboard() {
-  const { signOut } = useContext(AuthContext);
+  
 
   return (
     <div>
-      <h1>Dashboard</h1>
+      <h1>Lista de Pacientes</h1>
 
-      <button onClick={signOut}>Sair do Sistema</button>
+      
     </div>
   );
 }
